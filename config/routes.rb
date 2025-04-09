0861c89220c07e055321c5fd4ec1a3a0be24d1dd
klauslube <klauslube93@gmail.com>
2025-04-09 20:48:23 -0300

    chore: add configurations for devise, sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
